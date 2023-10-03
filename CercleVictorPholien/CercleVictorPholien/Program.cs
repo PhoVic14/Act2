961923200bdf5e90c51ad21ccf86f19838394908
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-10-03 14:31:01 +0200

    Ajout de l'ex1

---
+++ CercleVictorPholien/CercleVictorPholien/Program.cs
@@ -0,0 +1,27 @@
+﻿using CercleVictorPholien;
+using System;
+
+namespace CercleVictor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cercle cercle = new Cercle();
+
+            Console.WriteLine("Bienvenue dans ce programme sur le cercle");
+            do
+            {
+                Console.WriteLine("Tapez un rayon pour votre cercle");
+                cercle.Rayon = double.Parse(Console.ReadLine());
+                cercle.Caracteristique();
+                Console.WriteLine("Avec un rayon diminué de moitier");
+                cercle.Rayon /= 2;
+                cercle.Caracteristique();
+                Console.WriteLine("Un autre cercle ? (Tapez sur espace)");
+                Console.ReadLine();
+            } while (0 == 0);
+
+        }
+    }
+}
