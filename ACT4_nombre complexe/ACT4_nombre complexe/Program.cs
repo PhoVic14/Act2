63b6e176778bf039b1a38ca7407bada59e7e812e
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-10-15 20:23:49 +0200

    Ajout nombre Complexe

---
+++ ACT4_nombre complexe/ACT4_nombre complexe/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ACT4_nombre_complexe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complexe nbrComplexe = new Complexe();
+            Complexe newNbrComplexe = new Complexe();
+
+            Console.WriteLine("Bienvenue dans ce programme sur les complexes !");
+
+            do
+            {
+                Console.WriteLine("Que vaut la partie réelle du complexe de départ ?");
+                nbrComplexe.Reelle = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Que vaut la partie imaginaire du complexe de départ ?");
+                nbrComplexe.Imaginaire = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Le premier complexe : ");
+                nbrComplexe.AfficheComplexe();
+
+                Console.WriteLine(" et a pour module : " + nbrComplexe.CalculeModule());
+
+
+                
+                Console.WriteLine("Encodez un second nombre imaginaire : ");
+
+                Console.WriteLine("Que vaut la partie réelle du second complexe ?");
+                newNbrComplexe.Reelle = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Que vaut la partie imaginaire du second complexe ?");
+                newNbrComplexe.Imaginaire = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Le second module est :");
+                newNbrComplexe.AfficheComplexe();
+
+
+                nbrComplexe.Ajoute(newNbrComplexe.Reelle, newNbrComplexe.Imaginaire);
+                Console.WriteLine("Le premier complexe devient :");
+                nbrComplexe.AfficheComplexe();
+
+                Console.WriteLine("Un autr test ? (Tapez sur espace)");
+
+
+            } while (0 == 0);
+        }
+    }
+}
