055e07c61b0a08da7206d848fc8f09ab09df0d1c
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-10-15 20:25:00 +0200

    Ajout Sandwichs

---
+++ SandwichsVictorPholien/ACT4_Sandwichs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ACT4_Sandwichs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans notre concepteur de sandiwch ! ");
+            string value = "";
+            do
+            {
+                SandwichMaker sandwich = new SandwichMaker();
+                sandwich.composeSandwich();
+                Console.WriteLine("Tapez sur espace pour générer un sandwich, n'importe quelle touhce pour quitter ! :::");
+                value = Console.ReadLine();
+            } while (value == " ");
+
+            
+        }
+    }
+}
